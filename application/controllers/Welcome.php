b00fd1dab83dcbd481a9b56fec5627615f16b892
Maizatul Fatihah <norzaitolhanim74@gmail.com>
2019-10-22 01:59:36 +0700

    add new file that contain 'Hello World'

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+
+	public function index()
+	{
+		echo 'Hello World';
+	}
+
+	public function coba ()
+	{
+		echo 'Welcome / coba';
+	}
+}

31d4945ec26ac04d0f750ca0e677fb5e2d748337
Maizatul Fatihah <norzaitolhanim74@gmail.com>
2019-10-22 22:16:37 +0700

    add button 'Add Data' in file add.php

--- application/views/mahasiswa/add.php
+++ application/views/mahasiswa/add.php
@@ -7,19 +7,38 @@
             <div class="card-header">
                 Form Add Data Mahasiswa
             </div>
-            <div class="card-body">
-                <form action="" method="post">
-                    <div class="form-group">
-                        <label for="name">Name</label>
-                        <input type="text" class="form-control" id="name">
-                    </div>
-                </form>
+                <div class="card-body">
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control" id="name">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="matric no">Matric No</label>
+                            <input type="text" class="form-control" id="matric no">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" id="email">
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="course">Course</label>
+                            <select class="form-control" id="course" name="course">
+                                <option value="Software Engineering">Software Engineering</option>
+                                <option value="Artificial Intelligence">Artificial Intelligence</option>                        
+                                <option value="Information System">Information System</option>                        
+                                <option value="Networking">Networking</option>
+                                <option value="Data Science">Data Science</option> 
+                            </select>
+                        </div>
+                        <button type="submit" name="add" class="btn btn-primary float-right">Add Data</button>
+                    </form>
+                </div>
             </div>
-        </div>
         
-
-
-
         </div>
     
     </div>

c7b4b0deb3b1defa1dd5fca6251699346e96ce2d
Maizatul Fatihah <norzaitolhanim74@gmail.com>
2019-10-23 23:33:39 +0700

    create new file called pagination.php in order for the code to look more neat

--- application/controllers/People.php
+++ application/controllers/People.php
@@ -12,40 +12,11 @@ class People extends CI_Controller {
 
         //load library
         $this->load->library('pagination');
+
         //config
-        $config['base_url'] = 'http://localhost/rek-web/mahasiswa-CI/people/index';
         $config['total_rows'] = $this->people->countAllPeople();
         $config['per_page'] = 8;
-        $config['num_links'] = 5;
         
-        //styling
-        $config['full_tag_open'] = '<nav><ul class="pagination justify-content-center">';
-        $config['full_tag_close'] = '</ul></nav>';
-
-        $config['first_link'] = 'First';
-        $config['first_tag_open'] = '<li class="page-item">';
-        $config['first_tag_close'] = '</li>';
-       
-        $config['last_link'] = 'Last';
-        $config['last_tag_open'] = '<li class="page-item">';
-        $config['last_tag_close'] = '</li>';
-        
-        $config['next_link'] = '&raquo';
-        $config['next_tag_open'] = '<li class="page-item">';
-        $config['next_tag_close'] = '</li>';
-        
-        $config['prev_link'] = '&laquo';
-        $config['prev_tag_open'] = '<li class="page-item">';
-        $config['prev_tag_close'] = '</li>';
-        
-        $config['cur_tag_open'] = '<li class="page-item active"><a class="page-link" href="#">';
-        $config['cur_tag_close'] = '</a></li>';
-        
-        $config['num_tag_open'] = '<li class="page-item">';
-        $config['num_tag_close'] = '</li>';
-
-        $config['attributes'] = array('class' => 'page-link');
-
         //initialize
         $this->pagination->initialize($config);
 
